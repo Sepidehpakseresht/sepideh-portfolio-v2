7091b8c0cf7d7193a7e416d9679f7b1d8f602c1a
user1 <user1@MacBook-Pro.local>
2025-08-04 13:38:46 +0330

    🔧 Final updates: bug fixes, styling polish

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,30 @@
+import React, { Suspense } from 'react';
 import Header from './components/Header/Header';
-import Hero from './components/Hero';
 import AboutMe from './components/About-Me';
 import Skills from './components/Skills';
 import Works from './components/Works';
 import Experience from './components/Experienxe/Experience';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
-import { Suspense } from 'react';
+
+// Lazy load Hero
+const Hero = React.lazy(() => import('./components/Hero'));
 
 function App() {
   return (
     <div className="bg-background text-white min-h-screen overflow-x-hidden">
       <Header />
       <main>
-       <Suspense fallback={<div className="text-white text-center">🚀 Launching...</div>}></Suspense>
-        <Hero />
+        <Suspense fallback={<div className="text-white text-center">🚀 Launching...</div>}>
+          <Hero />
+        </Suspense>
         <AboutMe />
         <Skills />
         <Works />
         <Experience />
         <Contact />
       </main>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
